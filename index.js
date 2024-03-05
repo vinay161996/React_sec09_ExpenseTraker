c95df784b77cd29054e04698084c1b4b4bc6f3bb
vinay <rudravinay82@gmail.com>
2024-03-05 20:22:46 +0530

    addedincrementAndDecrement

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("yes");
+const redux = require("redux");
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  const type = action.type;
+  if (type === "incrementBy2") return { counter: state.counter + 2 };
+  if (type === "decrementBy2") return { counter: state.counter - 2 };
+  return {
+    counter: 0,
+  };
+};
+
+const store = redux.createStore(counterReducer);
+console.log("initial state", store.getState());
+const counterSubscribe = store.subscribe(() => {
+  console.log("Updated state", store.getState());
+});
+
+store.dispatch({ type: "incrementBy2" });
+store.dispatch({ type: "incrementBy2" });
+store.dispatch({ type: "incrementBy2" });
+store.dispatch({ type: "decrementBy2" });
+counterSubscribe();
