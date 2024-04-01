99962d0bbd34f63df8ec6dda4923ddbfab6c9496
vinay <rudravinay82@gmail.com>
2024-04-01 23:42:15 +0530

    firstCommit

--- src/features/calTotalExpenseAmount.js
+++
@@ -1,5 +0,0 @@
-function calTotalExpenseAmount(expenses) {
-  let totalAmount = expenses.reduce((prev, item) => prev + +item.amount, 0);
-  return totalAmount;
-}
-export default calTotalExpenseAmount;
--- src/pages/auth/AuthRootLayout.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
-
-const AuthRootLayout = () => {
-  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  return <>{isLoggedIn ? <Navigate to="/" /> : <Outlet />}</>;
-};
-
-export default AuthRootLayout;
