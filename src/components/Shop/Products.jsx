0de6acb874283bcbf282e56d4178b1e6104bfba5
vinay <rudravinay82@gmail.com>
2024-04-05 22:21:52 +0530

    updated

--- src/components/Shop/Products.jsx
+++ src/components/Shop/Products.jsx
@@ -1,15 +1,15 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
 
-const Products = (props) => {
+const Products = () => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
         <ProductItem
-          title='Test'
+          title="Test"
           price={6}
-          description='This is a first product - amazing!'
+          description="This is a first product - amazing!"
         />
       </ul>
     </section>
